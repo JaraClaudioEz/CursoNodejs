7218fe706e28cf0ac013495ef1769607c59a777a
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-04 17:07:58 -0300

    Completed basic module learning

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,5 @@
 
-function sayHello(name){
+function sayHello(name) {
     console.log('Hello ' + name); //console.log() Is a global object
 }
 
@@ -27,7 +27,7 @@ To be accessed the objects in a module need to be exported
 */
 
 const logger = require('./logger') //To load a module
-//for a sub folder './subFolder/logger', for a paretn folder '../logger'
+//for a sub folder './subFolder/logger', for a parent folder '../logger'
 //We can use jshint wich scan our javascript errors and gives back a more understable description
 //In terminal: jshint app.js
 
@@ -46,7 +46,66 @@ const os = require('os');
 let totalMemory = os.totalmem()
 let freeMemory = os.freemem()
 
-console.log(totalMemory + ' ' + freeMemory ) //With concatenation
+console.log(totalMemory + ' ' + freeMemory) //With concatenation
 console.log(`Total Memory: ${totalMemory}`); //With template string
 console.log(`Free Memory: ${freeMemory}`);
 
+//File System: all the methoods comes in pairs Synchronous or Asynchronous
+//Use allways Asynchronous methods!
+const fs = require('fs')
+/*
+const files = fs.readdirSync('./')
+console.log(files);
+*/
+
+fs.readdir('$', function (err, files) {
+    if (err) console.log('Error', err); //This is not how to treat errors normally
+    else console.log('Result', files);
+})
+
+//Events
+
+const EventEmitter = require('events') //EventEmitter is a Class, see naming convenction
+//const emitter = new EventEmitter() //emitter is an Object, a instance of EventEmitter. In the real world is quite rare to work with it directly
+
+/*
+//Raise an event
+emitter.emit('messageLogged', {id: 1, url: 'http://'}) //between {} is an object called Event Argument to pass data to a Listener
+//Important: first goes Listener then event raise, and event raise goes in logger module
+*/
+
+const Logger = require('./logger')
+const logger = new Logger()
+
+
+//Register a Listener
+emitter.on('messageLogged', (arg) => { // arg or argument or e or event
+    console.log('Listener called', arg);
+})
+
+logger.log('message')//Remember, when a event raise goes after the listener
+
+//HTTP Module
+const http = require('http')
+
+const server = http.createServer((req, resp) => {
+    if(req.url === '/'){ //To handle a url
+        resp.write('Hello world')
+        resp.end()
+    }
+
+    if(req.url === 'api/courses'){ //As we add more url the complexity raises, so we use Express framework to simplify 
+        resp.write(JSON.stringify([1, 2, 3])) //Using JSON we convert an array of numbers on string
+        resp.end()
+    }
+}) //With this we create a web server, this server is a EventEmitter
+
+/* This is a low level demostration
+server.on('connection', (socket)=> { //A listener or handler  
+    console.log('New connection...');
+})
+*/
+
+server.listen(3000)
+
+console.log('Listening on port 3000...'); //Every time there is a new connection or new request this server raises an event
--- first-app/logger.js
+++ first-app/logger.js
@@ -1,10 +1,20 @@
+const EventEmitter = require('events')
 
 const url = 'http://mylogger.io/log'
 
-function log(message){
+class Logger extends EventEmitter{
+    log(message) { //When a function is inside a Class is called Method, there is no need to use function reserved word
     //Send an HTTP request
     console.log(message);
+
+    //Raise an event
+    this.emit('messageLogged', { id: 1, url: 'http://' })
+}
 }
 
+/*
 module.exports.log = log; //We can define another name to the exported object ex. module.exports.mylog = log;
 //Also the line above exports an OBJECT, to export only a function: module.exports = log;
+*/
+
+module.exports = Logger
