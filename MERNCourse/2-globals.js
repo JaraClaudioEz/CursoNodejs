86dfba3b45bd6799d57c778d832fd1fc92dec63d
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-10 18:01:41 -0300

    Add globals.js

---
+++ MERNCourse/2-globals.js
@@ -0,0 +1,12 @@
+//GLOBALS - NO WINDOW!!!!
+//Some examples and most used
+//__dirname  - path to curretn directory
+//__filename - file name
+// require   - function to use modules (CommonJS)
+// module    - info about current module (file)
+// process   - info about enviroment where the program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+    console.log('Hello world!');
+}, 1000);
