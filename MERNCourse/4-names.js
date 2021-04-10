852f3a7d29c914811037a1418986d1ffdb1c0276
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-10 18:33:22 -0300

    Add Models files

---
+++ MERNCourse/4-names.js
@@ -0,0 +1,8 @@
+//Local
+const secret = 'SUPER SECRET'
+//Share
+const juan = 'Juan'
+const pedro = 'Pedro'
+
+//On ES6: export default
+module.exports = {juan, pedro} //exports an object
---
+++ MERNCourse/5-utils.js
@@ -0,0 +1,5 @@
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`);
+}
+
+module.exports = sayHi //No need of {} because only one item is exported
