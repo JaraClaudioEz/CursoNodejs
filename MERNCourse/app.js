3419793f97efa9e96edf64a2e9ea087867468b95
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-10 21:06:56 -0300

    Add OS module

---
+++ MERNCourse/app.js
@@ -0,0 +1,9 @@
+const {readFileSync, writeFileSync} = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(first, second);
+
+writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`)
+//We can add another argument: {flag: 'a'}, with this the file will be open and not overided by default
