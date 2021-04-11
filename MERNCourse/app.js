15d17a5e913be1ae357d5767a5587ff647f55355
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-10 21:08:16 -0300

    Add Path module

--- MERNCourse/app.js
+++ MERNCourse/app.js
@@ -6,4 +6,4 @@ const second = readFileSync('./content/second.txt', 'utf8');
 console.log(first, second);
 
 writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`)
-//We can add another argument: {flag: 'a'}, with this the file will be open and not overided by default
+//We can add another argument: {flag: 'a'}, with this the file will be open and not overided by default.
