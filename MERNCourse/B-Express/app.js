74ddfeff291abec77431952ec562905742d9baf0
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-18 11:49:25 -0300

    Handling url requests with node

--- MERNCourse/B-Express/app.js
+++ MERNCourse/B-Express/app.js
@@ -2,7 +2,10 @@ const http = require('http')
 const {readFileSync} = require('fs')
 
 // Get all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req, res) => { //This callback is invoked everytime the user hits our server
     //console.log('User hit the server');
@@ -19,6 +22,24 @@ const server = http.createServer((req, res) => { //This callback is invoked ever
     else if(url === '/about'){
         res.writeHead(200, { 'content-type': 'text/html' })
         res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    //Styles
+    else if(url === '/styles.css'){
+        res.writeHead(200, { 'content-type': 'text/css' })
+        res.write(homeStyles)
+        res.end()
+    }
+    //Logo
+    else if(url === '/logo.svg'){
+        res.writeHead(200, { 'content-type': 'image/svg+xml' })
+        res.write(homeImage)
+        res.end()
+    }
+    //Logic
+    else if(url === '/browser-app.js'){
+        res.writeHead(200, { 'content-type': 'text/javascript' })
+        res.write(homeLogic)
         res.end()
     }
     // 404
