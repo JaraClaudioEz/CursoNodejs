dbbaa1e1cbff058ca43cde992b2488383f05902e
Claudio Ezequiel Jara <claudiojara848@gmail.com>
2021-04-18 20:32:09 -0300

    Params and query string examples

--- MERNCourse/B-Express/app.js
+++ MERNCourse/B-Express/app.js
@@ -1,21 +1,56 @@
 const express = require('express');
-const path = require('path'); //To provide an absolute path
-
 const app = express();
+const { products } = require('./data')
 
-//Setup static and middleware, no need to setup statuses or content type, express takes care of all
-app.use(express.static('./public')) //public folder contains all the static resources that the app uses, are files that the server doesnt have to change
-
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html') There is two options to send index files
-//     1 - adding to static assets
-//     2 - SSR Server Side Rendering, with template engine
-// })
-
-app.all('*', (req, res)=>{
-    res.status(404).send('Resource not found')
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
 })
 
-app.listen(5000, ()=>{
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => { //To send specific parameters
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productID', (req, res) => { //Instead of hardcode the id we setup a route parameter using :myParamName
+    //console.log(req.params);
+    const { productID } = req.params;
+
+    const singleProduct = products.find((product) => prduct.id === Number(productID)) //productID need to be parsed to number becaues is a string
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+    return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => { //The route params can be more complex
+    console.log(req.params)
+    res.send('hello world')
+})
+
+app.get('/api/v1/query', (req, res) => {
+    //console.log(req.query);
+    const { search, limit } = req.query;
+    let sortedProducts = [...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length < 1) {
+        //res.status(200).send('No products matched your search') First alternative
+        return res.status(200).json({ success: true, data: [] }) //We are not giving a 404 status code because the search was successful but not returned anything
+    }
+    res.status(200).json(sortedProducts)
+
+})
+
+app.listen(5000, () => {
     console.log('Server is listening on port 5000...');
 })
--- MERNCourse/B-Express/final/7-params-query.js
+++ MERNCourse/B-Express/final/7-params-query.js
@@ -1,58 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const { products } = require('./data')
 
 app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
 })
+
 app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
-
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
-
-  return res.json(singleProduct)
+    const newProducts = products.map((product) => { //To send specific parameters
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+    res.json(newProducts)
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
+app.get('/api/products/:productID', (req, res) => { //Instead of hardcode the id we setup a route parameter using :myParamName
+    //console.log(req.params);
+    const { productID } = req.params;
+
+    const singleProduct = products.find((product) => prduct.id === Number(productID)) //productID need to be parsed to number becaues is a string
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+    return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => { //The route params can be more complex
+    console.log(req.params)
+    res.send('hello world')
 })
 
 app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
+    //console.log(req.query);
+    const { search, limit } = req.query;
+    let sortedProducts = [...products]
 
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length < 1) {
+        //res.status(200).send('No products matched your search') First alternative
+        return res.status(200).json({ success: true, data: [] }) //We are not giving a 404 status code because the search was successful but not returned anything
+    }
+    res.status(200).json(sortedProducts)
+
 })
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+    console.log('Server is listening on port 5000...');
 })
